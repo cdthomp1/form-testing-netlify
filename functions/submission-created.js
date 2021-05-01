0e4439b4f2a201d4a77b17669e52b4794a276f03
Cameron Thompson <camthomp96@gmail.com>
2021-04-30 23:18:44 -0600

    hmmm

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,8 +1,26 @@
-exports.handler = async function(event, context) {
-    console.log(event);
-    console.log(context);
+var nodemailer = require('nodemailer');
+var hbs = require('nodemailer-express-handlebars')
+
+const fs = require('fs');
+
+fs.readdir('/', (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
+
+exports.handler = async function (event, context) {
+    var formSub = JSON.parse(event.body);
+    console.log(formSub.payload.name)
+    var subscriber = {
+        name: formSub.payload.name,
+        email: formSub.payload.email
+    }
+
+    // sendWelcomeEmail(subscriber);
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({ message: `${formSub.payload.name}` })
     };
 }
