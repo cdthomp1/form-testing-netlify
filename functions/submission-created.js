dd26a4d4595b1b6de30b4634fb2a3271c0472b18
Cameron Thompson <camthomp96@gmail.com>
2021-04-30 23:25:26 -0600

    try this

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,7 +3,7 @@ var hbs = require('nodemailer-express-handlebars')
 
 const fs = require('fs');
 
-fs.readdir('/emails', (err, files) => {
+fs.readdir('/functions', (err, files) => {
     files.forEach(file => {
         console.log(file);
     });
